315ebe7ebcb1d1fb67e5c17485f42dededb7a1f5
Vimal Kant Yadav <vimalkantlive@gmail.com>
2023-04-28 21:36:52 +0530

    Changes in File Diff.java

--- Diff.java
+++ Diff.java
@@ -2,7 +2,6 @@ public class Diff {
     public static void main(String[] args){
         int a = 40;
         int b = 30;
-        System.out.println(a-b);
-        System.out.println(a+b);
+ 
     }
 }
