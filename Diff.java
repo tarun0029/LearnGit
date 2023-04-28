0d5ad43eee061a5653fbb046976c3231e0a94a24
Vimal Kant Yadav <vimalkantlive@gmail.com>
2023-04-28 19:32:29 +0530

    added new text to both files

--- Diff.java
+++ Diff.java
@@ -1,3 +1,7 @@
 public class Diff {
-    
+    public static void main(String[] args){
+        int a = 40;
+        int b = 30;
+        System.out.println(a-b);
+    }
 }
--- Sum.java
+++ Sum.java
@@ -1,5 +1,5 @@
 public class Sum{
-     public static void public static void main(String[] args) {
+     public static void  main(String[] args) {
          int a =10;
          int b= 30;
          System.out.print(a+b);
