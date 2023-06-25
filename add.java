faf62bc4edc9a00086203415174d8679976612f5
tarun0029 <tarunchandravanshiji@gmail.com>
2023-06-25 18:01:12 +0530

    created add.java file

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add{
+     public static void  main(String[] args) {
+         int a =20;
+         int b= 1000;
+         System.out.print(a+b);
+         System.out.println("Hello");
+     }
+}
